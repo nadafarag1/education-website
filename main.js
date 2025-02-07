438844231019d40ad910182b9b0d6a111cb94c6e
nadafarag1 <20201329@tiba.edu.eg>
2025-02-08 00:50:41 +0200

    fix bag

--- main.js
+++ main.js
@@ -23,3 +23,28 @@ faqs.forEach(faq => {
 
       });
     });
+
+
+
+
+    // show/hide nav menu
+    const menu = document.querySelector(".nav__menu");
+    const menuBtn = document.querySelector("#open-menu-btn");
+    const closeBtn = document.querySelector("#close-menu-btn");
+
+
+    menuBtn.addEventListener('click', () => {
+      menu.style.display = "flex"
+      closeBtn.style.display = "inline-block";
+      menuBtn.style.display = "none";
+    } );
+
+
+    //clse nav menu
+    const closeNav = () => {
+      menu.style.display = "none";
+      closeBtn.style.display = "none";
+      menuBtn.style.display = "inline-block";
+    }
+
+    closeBtn.addEventListener('click', closeNav);
